ffac9ba61ccf9071ce75535126b7788e65e4035c
Vu Pham <vdpham326@gmail.com>
2021-05-28 18:51:35 -0700

    more problems

---
+++ Part_2/AdvancedAstrology/src/AdvancedAstrology.java
@@ -0,0 +1,43 @@
+
+public class AdvancedAstrology {
+
+	public static void printStars(int number) {
+		for (int i = 0; i < number; i++) {
+			System.out.print("*");
+		}
+
+		System.out.println("");
+	}
+
+	public static void printSpaces(int number) {
+		for (int i = 0; i < number; i++) {
+			System.out.print(" ");
+		}
+	}
+
+	public static void printTriangle(int size) {
+		
+		for (int i = 1; i <= size; i++) {
+			printSpaces(size - i);
+			printStars(i);
+		}
+	}
+
+	public static void christmasTree(int height) {
+		for (int i = 1; i <= height; i++) {
+			printSpaces(height - i);
+			printStars(i * 2 - 1);
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			printSpaces(height - 2);
+			printStars(3);
+		}
+	}
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		christmasTree(11);
+	}
+
+}
---
+++ Part_2/StarSign/src/StarSign.java
@@ -0,0 +1,40 @@
+
+public class StarSign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		printStars(5);
+//		printStars(3);
+//		printStars(9);
+	//	printSquare(5);
+		
+		//printRectangle(17, 3);
+		printTriangle(4);
+	}
+	
+	public static void printStars(int number) {
+		for (int i = 0; i < number; i++) {
+			System.out.print("*");
+		}
+		
+		System.out.println("");
+	}
+	
+	public static void printSquare(int size) {
+		for (int i = 0; i < size; i++) {
+			printStars(size);
+		}
+	}
+	
+	public static void printRectangle(int width, int height) {
+		for (int i = 0; i < height; i++) {
+			printStars(width);
+		}
+	}
+	
+	public static void printTriangle(int size) {
+		for (int i = 1; i <= size; i++) {
+			printStars(i);
+		}
+	}
+}
